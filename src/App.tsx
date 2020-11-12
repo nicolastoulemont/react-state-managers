b7c6c1155c09417148821627ec0c0a67385f2c65
Nicolas Toulemont <n.toulemont@gmail.com>
2020-11-12 16:49:58 +0100

    basic layout to illustrate

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,30 @@ import React from 'react'
 import './App.css'
 
 export default function App() {
-	return <div className='App'>Coucou</div>
+	return (
+		<div className='app'>
+			<h1>Application</h1>
+			<Parent>
+				<Child name='Nicolas' />
+				<Child name='David' />
+			</Parent>
+		</div>
+	)
+}
+
+function Parent({ children }: { children: React.ReactNode }) {
+	return (
+		<div className='parent'>
+			<h1>Parent</h1>
+			<div className='children'>{children}</div>
+		</div>
+	)
+}
+
+function Child({ name }: { name: string }) {
+	return (
+		<div className={`child ${name}`}>
+			<h1>Child: {name}</h1>
+		</div>
+	)
 }
