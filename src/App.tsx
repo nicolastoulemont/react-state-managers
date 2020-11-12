1a63cff8b045f0fac70ab338abbe9471e93a77c8
Nicolas Toulemont <n.toulemont@gmail.com>
2020-11-12 17:31:22 +0100

    basic state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
+const BLUE_BG_COLOR = '#90CDF4'
+const RED_BG_COLOR = '#FEB2B2'
+
 export default function App() {
+	console.log('Parent rendering')
 	return (
 		<div className='app'>
 			<h1>Application</h1>
@@ -14,6 +18,7 @@ export default function App() {
 }
 
 function Parent({ children }: { children: React.ReactNode }) {
+	console.log('Parent rendering')
 	return (
 		<div className='parent'>
 			<h1>Parent</h1>
@@ -23,9 +28,17 @@ function Parent({ children }: { children: React.ReactNode }) {
 }
 
 function Child({ name }: { name: string }) {
+	const [bgColor, setBgColor] = useState('white')
+
+	console.log(`Child ${name} rendering`)
+
 	return (
-		<div className={`child ${name}`}>
+		<div className={`child ${name}`} style={{ backgroundColor: bgColor }}>
 			<h1>Child: {name}</h1>
+			<div>
+				<button onClick={() => setBgColor(RED_BG_COLOR)}>Rouge</button>
+				<button onClick={() => setBgColor(BLUE_BG_COLOR)}>Bleu</button>
+			</div>
 		</div>
 	)
 }
